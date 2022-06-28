4fcf4e279fd4d780c800af3319d4a4e662781187
Issachar Vinajeras <issacharv@gmail.com>
2022-06-27 20:50:27 -0400

    update material init

--- app/materials/__init__.py
+++ app/materials/__init__.py
@@ -1,3 +1,4 @@
-from materials.material import Material
+from material import Material
 from materials.base_material import BaseMaterial
+from materials.crafted_material import CraftedMaterial
 from materials.ingredients import Ingredient
