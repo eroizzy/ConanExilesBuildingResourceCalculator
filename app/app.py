37b9b483b66b755dc65e3b685120bbded12ac64a
Issachar Vinajeras <issacharv@gmail.com>
2022-06-27 22:52:44 -0400

    moving app

--- app/app.py
+++ app/app.py
@@ -1,14 +1,13 @@
-from materials.base_material import BaseMaterial
+from app.materials.base_material import BaseMaterial
 
 
-class test:
+class Test:
     def print_resource_tree(self) -> None:
         pass
 
     def test(self):
         print(BaseMaterial("Ironstone"))
-        pass
 
 
 if __name__ == "__main__":
-    test().test()
+    Test().test()
