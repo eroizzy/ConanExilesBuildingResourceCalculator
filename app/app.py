781a7be632e69addf0b94678c41e1f17b087e30f
Issachar Vinajeras <issacharv@gmail.com>
2022-06-27 20:12:22 -0400

    clear app.py

--- app/app.py
+++ app/app.py
@@ -1,28 +1,15 @@
-from materials.black_ice.black_ice_wall import black_ice_wall, material_dict, material
 
+
+
+from materials import Material
 
 class test():
-    def print_resource_tree(self, mat: material_dict, spacer: int=0) -> None:
-        this_material = mat["mat"]
-        qty = mat["qty"]
-        indent= "".join(["\t" for x in range(spacer)])
-        decorator: str = f'- { qty } '
-        print(f'{ indent }{ decorator }{ this_material.name }')
-        if not this_material.is_base_material():
-            for m in this_material.get_needed_material():
-                self.print_resource_tree(m, spacer=spacer+1)
+    def print_resource_tree(self) -> None:
+        pass
     
     def test(self):
-        mat: black_ice_wall = black_ice_wall()
-
-        self.print_resource_tree(material_dict(mat=mat,qty=1))
-
-        # for x in mat.get_needed_material():
-        #     temp: material = x["mat"]
-        #     if not temp.is_base_material():
-        #         print(temp.get_needed_material())
-        #     else:
-        #         print("Base Material")
+        print("Hello World")
+        pass
 
 
 if __name__ == "__main__":
