d8fa8919c143b8fa4be0e5a8c8c5d596b7c3e96f
Issachar Vinajeras <issacharv@gmail.com>
2022-06-27 22:49:22 -0400

    updating base_material

--- app/materials/base_material.py
+++ app/materials/base_material.py
@@ -1,9 +1,11 @@
-from . import Material
+from abc import abstractmethod
+from app.materials.material import Material
 
 
 class BaseMaterial(Material):
     def __init__(self, name: str, base_material: bool = True) -> None:
         super().__init__(name, base_material)
 
-    def get_needed_material(self) -> None:
+    @abstractmethod
+    def get_ingredient_list(self) -> None:
         return None
